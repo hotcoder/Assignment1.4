83c34e31d76163a477ae65e82a26cef2f3cc0a5f
hotcoder <hotcoder@users.noreply.github.com>
2017-11-13 09:25:08 +0530

    volume of a sphere with diameter 12 cm
    volume of a sphere with diameter 12 cm

---
+++ Assignment1.4.py
@@ -0,0 +1,12 @@
+"""
+Python program to find the volume of a sphere with diameter 12 cm.
+"""
+
+import math
+
+diameter = 12
+
+sphere_volume = ((4/3)*math.pi*(diameter/2)**3)
+
+
+print("volume of a sphere : ", sphere_volume)
